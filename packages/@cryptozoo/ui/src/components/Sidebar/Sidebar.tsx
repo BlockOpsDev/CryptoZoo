ea7770d143ab87c4da29cabbbf805a61e18d6728
Spencer McWilliams <spencer.mcw@gmail.com>
2022-08-03 15:53:57 -0700

    Introducing icons to nav items

--- packages/@cryptozoo/ui/src/components/Sidebar/Sidebar.tsx
+++ packages/@cryptozoo/ui/src/components/Sidebar/Sidebar.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
 
+import { AiFillHome, AiOutlineSwap } from 'react-icons/ai'
+import { IoStorefront } from 'react-icons/io5'
+
+
+const ICON_SIZE = "30"
+
+const iconMap: {
+  [key: string]: Element
+} = {
+  'home': <AiFillHome size={ICON_SIZE} />,
+  'swap': <AiOutlineSwap size={ICON_SIZE} />,
+  'store': <IoStorefront size={ICON_SIZE} />,
+}
+
+
 export interface NavItem {
   title: string;
   to: string;
-  iconClass?: string;
+  iconKey: string;
   locked: boolean;
 }
 
@@ -12,34 +27,44 @@ export interface SidebarProps {
   navItems: NavItem[];
 }
 
+const generateNavItem = (navItem: NavItem) => {
+  return (
+    <Link href={navItem.to}>
+      <div className="h-12 w-12 bg-layer--3 text-primary-text rounded-lg flex justify-center items-center">
+        { iconMap[navItem.iconKey] }
+      </div>
+    </Link>
+  );
+}
+
 export const Sidebar: React.FC<SidebarProps> = ({ navItems }: SidebarProps) => {
   return (
-    <div className="text-primary-text max-w-screen divide-layer--3 fixed top-0 left-0 flex h-screen flex-col divide-y-2">
-      <div className="p-4">ZOO</div>
-      <nav className="flex flex-col gap-4">
-        <ul>
-          {navItems.map((navItem: NavItem, index: number) => {
-            return (
-              <li key={index} className="bg-layer--2 rounded-lg">
-                <Link href={navItem.to}>{navItem.title}</Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
+    <div className="text-primary-text max-w-screen divide-layer--3 fixed top-0 left-0 flex h-screen flex-col  border-r-2 border-r-layer--2">
+      <div className="p-4 border-b-2 border-b-layer--2">ZOO</div>
+      <ul className="flex flex-col gap-4 mt-4">
+        {navItems.map((navItem: NavItem, index: number) => {
+          return (
+            <li key={index} className="w-fit mx-auto">
+              {generateNavItem(navItem)}
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
 
+
 Sidebar.defaultProps = {
   navItems: [
-    { title: 'Home', to: '#', locked: false },
-    { title: 'ZooSwap', to: '#', locked: false },
-    { title: 'Marketplace', to: '#', locked: true },
-    { title: 'Breeding/Hatching', to: '#', locked: true },
-    { title: 'Rewards', to: '#', locked: true },
-    { title: 'Games', to: '#', locked: true },
-    { title: 'Links/Resources', to: '#', locked: true },
-    { title: 'Discord', to: '#', locked: true },
+    { title: 'Home', to: '#', locked: false, iconKey: 'home' },
+    { title: 'ZooSwap', to: '#', locked: false, iconKey: 'swap' },
+    { title: 'Marketplace', to: '#', locked: true, iconKey: 'store' },
+    { title: 'Breeding/Hatching', to: '#', locked: true, iconKey: 'home' },
+    { title: 'Rewards', to: '#', locked: true, iconKey: 'home' },
+    { title: 'Games', to: '#', locked: true, iconKey: 'home' },
+    { title: 'Links/Resources', to: '#', locked: true, iconKey: 'home' },
+    { title: 'Discord', to: '#', locked: true, iconKey: 'home' },
   ],
 };
+
