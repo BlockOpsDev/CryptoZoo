ddf726423d42ef4adfcfa78e77e79943764e774a
xSNAGSx <xsnagsx@protonmail.com>
2022-08-03 12:48:46 -0500

    Add Tailwind Color Config

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,4 +1,4 @@
-const colors = require('tailwindcss/colors');
+
 
 module.exports = {
   content: [
@@ -8,12 +8,31 @@ module.exports = {
     // "../../packages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        brandblue: colors.blue[500],
-        brandred: colors.red[500],
-      },
-    },
+    colors: {
+      'layer--1': '#0e1324',
+      'layer--2': '#161e31',
+      'layer--3': '#212b3f',
+      'layer--4': '#2e394d',
+
+      'primary-text': '#ffffff',
+      'secondary-text': '#94a3b8',
+
+      'primary--light': '#55a6ed',
+      'primary': '#0062d4',
+      'primary--dark': '#002a7a',
+
+      'success--light': '#2ec03e',
+      'success': '#00741a',
+      'success--dark': '#002f0f',
+
+      'error--light': '#e4877c',
+      'error': '#bb352b',
+      'error--dark': '#680300',
+
+      'warn--light': '#c09d43',
+      'warn': '#9b5e00',
+      'warn--dark': '#512900',
+    }
   },
   darkMode: 'class',
   plugins: [],
