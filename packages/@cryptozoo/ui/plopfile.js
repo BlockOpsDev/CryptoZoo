ade41e79c14b484ad3533cadcec2fe8324df4db7
Pucarak <pucarak@protonmail.com>
2022-08-03 10:27:22 -0500

    Component Type Templates

--- packages/@cryptozoo/ui/plopfile.js
+++ packages/@cryptozoo/ui/plopfile.js
@@ -1,41 +1,68 @@
 module.exports = (plop) => {
-  plop.setGenerator('FragC', {
-    description: 'Create a reusable Fragment Component',
+  plop.setHelper('upperCase', (txt) => txt.toUpperCase());
+
+  plop.setGenerator('Component', {
+    description: 'Create a reusable Component',
     prompts: [
+      {
+        type: 'list',
+        name: 'type',
+        message: 'What is your component type?',
+        choices: ['Base', 'Fragment', 'Query'],
+      },
       {
         type: 'input',
         name: 'name',
         message: 'What is your component name?',
       },
     ],
-    actions: [
-      {
-        type: 'add',
-        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
-        templateFile: 'plop-templates/Component/Component.tsx.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
-        templateFile: 'plop-templates/Component/Component.stories.tsx.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/components/{{pascalCase name}}/index.ts',
-        templateFile: 'plop-templates/Component/index.ts.hbs',
-      },
-      {
-        type: 'add',
-        path: 'src/components/index.tsx',
-        templateFile: 'plop-templates/injectable-index.tsx.hbs',
-        skipIfExists: true,
-      },
-      {
-        type: 'append',
-        path: 'src/components/index.tsx',
-        pattern: `/* PLOP_INJECT_EXPORT */`,
-        template: `export * from './{{pascalCase name}}';`,
-      },
-    ],
+    actions: function (data) {
+      const componentAction = (template) => {
+        return {
+          type: 'add',
+          path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+          templateFile: `plop-templates/Component/${template}`,
+        };
+      };
+
+      const actions = [
+        {
+          type: 'add',
+          path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+          templateFile: 'plop-templates/Component/Component.stories.tsx.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/components/{{pascalCase name}}/index.ts',
+          templateFile: 'plop-templates/Component/index.ts.hbs',
+        },
+        {
+          type: 'add',
+          path: 'src/components/index.tsx',
+          templateFile: 'plop-templates/injectable-index.tsx.hbs',
+          skipIfExists: true,
+        },
+        {
+          type: 'append',
+          path: 'src/components/index.tsx',
+          pattern: `/* PLOP_INJECT_EXPORT */`,
+          template: `export * from './{{pascalCase name}}';`,
+        },
+      ];
+
+      switch (data.type) {
+        case 'Base':
+          actions.push(componentAction('Component.tsx.hbs'));
+          break;
+        case 'Fragment':
+          actions.push(componentAction('FragComponent.tsx.hbs'));
+          break;
+        case 'Query':
+          actions.push(componentAction('QueryComponent.tsx.hbs'));
+          break;
+      }
+
+      return actions;
+    },
   });
 };
