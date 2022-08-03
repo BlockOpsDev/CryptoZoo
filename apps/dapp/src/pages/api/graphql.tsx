a92a06c8c0048f76fa6f8f7a2a4e17898b852254
Pucarak <pucarak@protonmail.com>
2022-08-03 07:20:58 -0500

    Moralis Connection

--- apps/dapp/src/pages/api/graphql.tsx
+++ apps/dapp/src/pages/api/graphql.tsx
@@ -1,10 +1,17 @@
 import Cors from 'micro-cors';
 import { PageConfig } from 'next';
 import { ApolloServer } from 'apollo-server-micro';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import Moralis from 'moralis/node';
 
 // @ts-ignore
 import typeDefs from '@cryptozoo/graphql/schema.graphql';
 import { resolvers } from '@cryptozoo/graphql';
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers,
+});
 
 export const config: PageConfig = {
   api: {
@@ -14,7 +21,19 @@ export const config: PageConfig = {
 
 const cors = Cors();
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  schema,
+  context: async () => {
+    if (!Moralis.isInitialized)
+      await Moralis.start({
+        serverUrl: process.env.MORALIS_SERVER,
+        appId: process.env.MORALIS_APP_ID,
+        masterKey: process.env.MORALIS_MASTER_KEY,
+      });
+
+    return { Moralis };
+  },
+});
 
 const startServer = server.start();
 
