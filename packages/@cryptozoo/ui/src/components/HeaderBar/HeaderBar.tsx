017ff69302231fe7ab813ec3ab36fa79130b3be0
Spencer McWilliams <spencer.mcw@gmail.com>
2022-08-03 13:41:56 -0700

    HeaderBar content

--- packages/@cryptozoo/ui/src/components/HeaderBar/HeaderBar.tsx
+++ packages/@cryptozoo/ui/src/components/HeaderBar/HeaderBar.tsx
@@ -1,9 +1,10 @@
 export const HeaderBar = () => {
   return (
-    <div className="w-full flex">
-      <div className=""></div>
+    <div className="w-full flex bg-layer--1 p-4 gap-4">
+      <div className="flex-grow">
+      </div>
       <div className="flex-none">
-        <button className=""></button>
+        <button className="px-4 py-2 bg-primary rounded-lg text-primary-text">Wallet Button Placeholder</button>
       </div>
     </div>
   )
