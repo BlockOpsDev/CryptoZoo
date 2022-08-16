a1dec53f2f1f7f11ef075a5144e5b4a39a0156fd
Spencer McWilliams <spencer.mcw@gmail.com>
2022-08-16 14:57:07 -0700

    Refactor Apollo Examples

--- packages/@cryptozoo/ui/src/components/index.tsx
+++ packages/@cryptozoo/ui/src/components/index.tsx
@@ -1,8 +1,9 @@
 /* PLOP_INJECT_EXPORT */
 export * from './NavLink';
 export * from './Layout';
-export * from './QueryComponentExample';
-export * from './FragmentComponentExample';
 export * from './Button';
 export * from './Card';
 export * from './Test';
+
+export * from './Apollo/QueryComponentExample';
+export * from './Apollo/FragmentComponentExample';
